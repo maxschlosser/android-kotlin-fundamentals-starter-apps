3933c8cdd4b3bf24c24979abd3af2c8a27ddf4fb
maxschlosser <maxschlosser96@googlemail.com>
2020-11-21 08:04:03 +0100

    progress, before livedata

--- AboutMeInteractive-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ AboutMeInteractive-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -14,19 +14,40 @@
  * limitations under the License.
  */
 
-package com.android.example.aboutme
+package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.android.example.aboutme.R
+
 /**
  * Main Activity of the AboutMe app. This app demonstrates:
  *     * EditText
  *     * Entering, getting, and displaying text.
  */
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var doneButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doneButton = findViewById(R.id.done)
+        doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+    private fun addNickname(view: View) {
+        val editNickname = findViewById<EditText>(R.id.nickname_edit)
+        val showNickname = findViewById<TextView>(R.id.nickname_text)
+        showNickname.text = editNickname.text
+        showNickname.visibility = View.VISIBLE
+        editNickname.visibility = View.GONE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
